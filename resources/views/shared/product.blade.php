1a6820e04b1acfc4539cb1d95665962710be0a68
Rachele-Turtora <racheleturtora21@gmail.com>
2024-07-08 18:54:22 +0200

    [UPD] heart

--- resources/views/shared/product.blade.php
+++ resources/views/shared/product.blade.php
@@ -1,4 +1,7 @@
-<img src="{{ Vite::asset('resources/img/' . $product['frontImage']) }}" alt="Img-{{ $product['id'] }}">
+<div class="img-product">
+    <img src="{{ Vite::asset('resources/img/' . $product['frontImage']) }}" alt="Img-{{ $product['id'] }}">
+    <div class="heart @if($product['isInFavorites']) heart-favourite @endif"><i class="fa-solid fa-heart"></i></div>
+</div>
 <p>{{$product['brand']}}</p>
 <p class="text-uppercase fw-bold">{{$product['name']}}</p>
 <span class="new-price"></span>
